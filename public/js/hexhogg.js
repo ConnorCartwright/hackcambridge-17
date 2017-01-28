a9bd971be3ede7a0a31d1add9b6623f1992e6e5c
clegganator259 <djclegg01@hotmail.com>
2017-01-28 17:38:17 +0000

    Adding update method to pulses

--- public/js/hexhogg.js
+++ public/js/hexhogg.js
@@ -1,8 +1,24 @@
 var bpm = 120;
 var period = 60000 / bpm;
+var pulses = [];
 
 function update(){
-  console.log("HOOOOOI");
+  for(var i = 0; i < pulses.length; i++){
+    pulses[i].update();
+  }
+}
+
+function makePulse(x,y,updateVector){
+  var pulse = {
+    x: x,
+    y: y,
+    vector: updateVector,
+    update: function(){
+      this.x += updateVector.x;
+      this.y += updateVector.y;
+    }
+  }
+  pulses.push(pulse);
 }
 
 var timer = setInterval(period,update);
