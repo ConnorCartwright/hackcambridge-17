ba644712c993c1082323a52fde5c553a2c49d370
Connor Cartwright <connorcart@gmail.com>
2017-01-29 02:32:14 +0000

    Add resize file

---
+++ public/js/resize.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    function resizeContainers() {
+        var containerWidth = $('.container').width();
+        var toolbarWidth = $('.toolbarContainer').width();
+        var margin = 10;
+        $('.hexContainer').width((containerWidth - toolbarWidth) - margin);
+        generateHexGrid();
+    }
+
+
+   $(window).on('resize', function() {
+       resizeContainers();
+   });
+
+    $(window).on('load', function() {
+        resizeContainers();
+    });
+
+
+
+
+
+});
