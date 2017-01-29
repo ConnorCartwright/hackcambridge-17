27e1aae351cd1af7a803db1311f136a0667d5d3a
Gwendolyn Wong <gsw371@bham.ac.uk>
2017-01-29 08:31:28 +0000

    Updated the toolbar so that buttons now temporarily hardcods a value and further send it to the DB

--- public/js/firebaseCRUD.js
+++ public/js/firebaseCRUD.js
@@ -3,3 +3,30 @@ var getTableData = function(currTable, callback) {
       callback(snapshot.val());
     });
 }
+
+var onTableDataChange = function(child, callback) {
+    var dbRef = firebase.database().ref().child(child);
+    
+    dbRef.on("value", function(snapshot){
+        console.log("value changed in tiles!");
+        console.log(snapshot.val());
+        callback(snapshot.val());
+    })
+}
+
+
+//REPLACES EVERYTHING IN THE TABLE WITH THIS DATA PROVIDED!
+var setTableData = function(currTable, data) {
+    return firebase.database().ref('/'+currTable).set(data);
+}
+
+//adds tiles to the preexisting list
+var addTile = function(coordinates, data) {
+    var currTable = 'Tiles';
+    var newPostKey = firebase.database().ref().child(currTable).push().key;
+    var updates = {};
+    
+    updates['/'+currTable+'/'+coordinates] = data;
+    firebase.database().ref('/'+currTable+'/'+coordinates).set(data);
+    return firebase.database().ref().update(updates);
+}
--- public/js/toolbar.js
+++ public/js/toolbar.js
@@ -1,4 +1,109 @@
 $('.toolbarTool').on('click', function() {
     $('.toolbarTool').removeClass('active');
     $(this).addClass('active');
+    
+    
+    var currTime = getDateTime();
+    var nodetype = getType(this);
+    console.log(nodetype);
+    
+    
+    //creates a json object when a button is selected, ready to be assigned to the database
+//    var json_created = {
+//        "creator" : "Gwen",
+//        "direction" : "DOWN",
+//        "editor" : ["Gwen"],
+//        "pulsePerBeat" : 1,
+//        "time_created" : currTime,
+//        "time_edited" : currTime,
+//        "type_name" : type
+//    };
+//    assigns this to currSelectedNode (global variable)
+//    currSelectedNode = json_created;
+//    console.log(currSelectedNode);
+    
+    
+    //DEBUG PURPOSE
+    
+    onTableDataChange("Tiles", function(){});
+    
+    //for ease, now when we click on a button is sets it to a predefined coordinate
+    if($(this).hasClass("startNode")) {
+        var json_created = {
+            "creator" : "Gwen",
+            "direction" : "UP",
+            "editor" : ["Gwen"],
+            "pulsePerBeat" : 1,
+            "time_created" : currTime,
+            "time_edited" : currTime,
+            "type_name" : nodetype
+        };
+
+        currSelectedNode = json_created;
+        console.log(currSelectedNode);
+        
+        
+    
+        addTile("6,3", json_created);
+    } else if($(this).hasClass("stopNode")) {
+        var json_created = {
+            "creator" : "Gwen",
+            "direction" : "DOWN",
+            "editor" : ["Gwen"],
+            "pulsePerBeat" : 1,
+            "time_created" : currTime,
+            "time_edited" : currTime,
+            "type_name" : nodetype
+        };
+
+        currSelectedNode = json_created;
+        console.log(currSelectedNode);
+        addTile("6,-3", json_created);
+    } else if($(this).hasClass("ricochetNode")) {
+        var json_created = {
+            "creator" : "Gwen",
+            "direction" : "UPPERLEFT",
+            "editor" : ["Gwen"],
+            "pulsePerBeat" : 1,
+            "time_created" : currTime,
+            "time_edited" : currTime,
+            "type_name" : nodetype
+        };
+
+        currSelectedNode = json_created;
+        console.log(currSelectedNode);
+        addTile("6,-1", json_created);
+    }
 });
+
+var getType = function(thisClass) {
+    var type = "";
+    
+    if($(thisClass).hasClass("startNode")) {
+        type = "Start";
+    } else if($(thisClass).hasClass("stopNode")) {
+        type = "Stop";
+    } else if($(thisClass).hasClass("ricochetNode")) {
+        type = "Richochet";
+    }
+    
+    return type;
+}
+
+var getDateTime = function() {
+    var date = new Date();
+    var d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+    var formatD = dformat = [d.getFullYear(),
+                            (d.getMonth()+1).padLeft(),
+                             d.getDate().padLeft()
+                            ].join('/') +' ' +
+                            [d.getHours().padLeft(),
+                             d.getMinutes().padLeft(),
+                             d.getSeconds().padLeft()].join(':');
+    return formatD;
+}
+
+Number.prototype.padLeft = function(base,chr){
+    var  len = (String(base || 10).length - String(this).length)+1;
+    return len > 0? new Array(len).join(chr || '0')+this : this;
+}
